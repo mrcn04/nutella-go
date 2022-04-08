04c4ba0fea9f7063c171710fa394e5dca15280b9
mrcn04 <dodomrcn@gmail.com>
2022-04-08 15:26:28 +0300

    handler func and api structs

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello from nutella go")
+	fmt.Println("Starting the server...")
+
+	http.HandleFunc("/cache", Handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("in the handler")
+
+	resp := APIResponse{
+		Cache: false,
+		Data:  make([]NominatimResponse, 0),
+	}
+
+	err := json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		fmt.Printf("error encoding response: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+type APIResponse struct {
+	Cache bool                `json:"cache"`
+	Data  []NominatimResponse `json:"data"`
+}
+
+type NominatimResponse struct {
+	PlaceID     int      `json:"place_id"`
+	Licence     string   `json:"licence"`
+	OsmType     string   `json:"osm_type"`
+	OsmID       int      `json:"osm_id"`
+	Boundingbox []string `json:"boundingbox"`
+	Lat         string   `json:"lat"`
+	Lon         string   `json:"lon"`
+	DisplayName string   `json:"display_name"`
+	Class       string   `json:"class"`
+	Type        string   `json:"type"`
+	Importance  float64  `json:"importance"`
+	Icon        string   `json:"icon"`
 }
