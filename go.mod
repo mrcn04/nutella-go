0e94b0767d269fe4f0b1176e392747e0ecde7e3e
mrcn04 <dodomrcn@gmail.com>
2022-04-13 17:14:09 +0300

    ..

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module nutella-go
+module github.com/mrcn04/nutella-go
 
 go 1.17
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
@@ -15,14 +16,22 @@ func main() {
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("in the handler")
+	fmt.Println("**** in the handler ****")
+
+	q := r.URL.Query().Get("q")
+	data, err := getData(q)
+	if err != nil {
+		fmt.Printf("error calling data source: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	resp := APIResponse{
 		Cache: false,
-		Data:  make([]NominatimResponse, 0),
+		Data:  data,
 	}
 
-	err := json.NewEncoder(w).Encode(resp)
+	err = json.NewEncoder(w).Encode(resp)
 	if err != nil {
 		fmt.Printf("error encoding response: %v\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -30,22 +39,20 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type APIResponse struct {
-	Cache bool                `json:"cache"`
-	Data  []NominatimResponse `json:"data"`
-}
+func getData(q string) ([]NominatimResponse, error) {
+	escapedQ := url.PathEscape(q)
+	address := fmt.Sprintf("https://nominatim.openstreetmap.org/search?q=%s&format=json", escapedQ)
 
-type NominatimResponse struct {
-	PlaceID     int      `json:"place_id"`
-	Licence     string   `json:"licence"`
-	OsmType     string   `json:"osm_type"`
-	OsmID       int      `json:"osm_id"`
-	Boundingbox []string `json:"boundingbox"`
-	Lat         string   `json:"lat"`
-	Lon         string   `json:"lon"`
-	DisplayName string   `json:"display_name"`
-	Class       string   `json:"class"`
-	Type        string   `json:"type"`
-	Importance  float64  `json:"importance"`
-	Icon        string   `json:"icon"`
+	resp, err := http.Get(address)
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]NominatimResponse, 0)
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
